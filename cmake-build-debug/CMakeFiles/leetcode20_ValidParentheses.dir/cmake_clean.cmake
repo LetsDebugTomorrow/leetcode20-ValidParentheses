dcca96b3b9c65321078ada8503e18bda7f99d961
AllenTang <1020145095@qq.com>
2022-01-28 18:31:49 +0800

    1.0

---
+++ cmake-build-debug/CMakeFiles/leetcode20_ValidParentheses.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode20_ValidParentheses.dir/main.cpp.o"
+  "leetcode20_ValidParentheses"
+  "leetcode20_ValidParentheses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode20_ValidParentheses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
